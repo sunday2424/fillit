0f29f995a1aed3aff2deae289399f9053f96a63e
Junsik Park <junpark@e1z2r7p1.42.us.org>
2019-04-15 17:16:03 -0700

    .

---
+++ src/lstfn.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lstfn.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junpark <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/14 08:59:55 by junpark           #+#    #+#             */
+/*   Updated: 2019/04/15 17:11:35 by junpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+/*
+** list functions that are used but these were not
+** required functions from libft.
+*/
+
+size_t	ft_lstcount(t_list *lst)
+{
+	size_t i;
+
+	i = 0;
+	while (lst != NULL)
+	{
+		lst	= lst->next;
+		i++;
+	}
+	return (i);
+}
+
+void	ft_lstrev(t_list **alst)
+{
+	t_list	*prev;
+	t_list	*cur;
+	t_list	*next;
+
+	prev = NULL;
+	cur = *alst;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*alst = prev;
+}
+
+/*
+** subset function of read_maplst.
+*/
+
+t_list	*free_list(t_list *list)
+{
+	t_tetris	*tetris;
+	t_list		*next;
+
+	while (list)
+	{
+		tetris = (t_tetris *)list->content;
+		next = list->next;
+		free_tetris(tetris);
+		ft_memdel((void **)&list);
+		list = next;
+	}
+	return (NULL);
+}
