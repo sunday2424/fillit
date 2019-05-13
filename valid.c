8e10bd93d06a01b6a25fe6f8c1ef506c0df9df23
Junsik Park <junpark@e1z2r6p1.42.us.org>
2019-05-13 11:27:29 -0700

    .

--- valid.c
+++ valid.c
@@ -38,3 +38,31 @@ int		count_blocks(char **map)
 	}
 	return ((d == 12 && s == 4) ? 1 : 0);
 }
+
+int		count_connection(char **map)
+{
+	int	x;
+	int	y;
+	int	count;
+
+	y = 0;
+	count = 0;
+	while (y < 4)
+	{
+		x = 0;
+		while (x < 5)
+		{
+			if ((x + 1) < 5 && map[y][x + 1] == '#')
+				count++;
+			if ((x - 1) >= 0 && map[y][x - 1] == '#')
+				count++;
+			if ((y + 1) < 4 && map[y + 1][x] == '#')
+				count++;
+			if ((y - 1) >= 0 && map[y - 1][x] == '#')
+				count++;
+			x++;
+		}
+		y++;
+	}
+	return ((count == 6 || count == 8) ? 1 : 0);
+}
