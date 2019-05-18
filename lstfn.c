7e9514f4a55fc51ce5e456aecc1bece147763bed
Junsik Park <junpark@e1z2r7p19.42.us.org>
2019-05-17 23:31:46 -0700

    .

--- lstfn.c
+++ lstfn.c
@@ -17,62 +17,34 @@
 ** required functions from libft.
 */
 
-size_t	ft_lstcount(t_list *lst)
+size_t    tet_lstcount(t_tet *list)
 {
-	size_t i;
-
-	i = 0;
-	while (lst != NULL)
-	{
-		lst	= lst->next;
-		i++;
-	}
-	return (i);
+    size_t i;
+    
+    i = 0;
+    while (list != NULL)
+    {
+        list = list->next;
+        i++;
+    }
+    return (i);
 }
 
-void	ft_lstrev(t_list **alst)
+void    tet_lstprev(t_tet **alst)
 {
-	t_list	*prev;
-	t_list	*cur;
-	t_list	*next;
-
-	prev = NULL;
-	cur = *alst;
-	while (cur != NULL)
-	{
-		next = cur->next;
-		cur->next = prev;
-		prev = cur;
-		cur = next;
-	}
-	*alst = prev;
+    t_tet    *prev;
+    t_tet    *cur;
+    t_tet    *next;
+    
+    prev = NULL;
+    cur = *alst;
+    while (cur != NULL)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    *alst = prev;
 }
 
-/*
-** subset function of read_maplst.
-*/
-
-t_list	*free_list(t_list *list)
-{
-	t_tetris	*tetris;
-	t_list		*next;
-
-	while (list)
-	{
-		tetris = (t_tetris *)list->content;
-		next = list->next;
-		free_tetris(tetris);
-		ft_memdel((void **)&list);
-		list = next;
-	}
-	return (NULL);
-}
-
-void	ft_lstadd(t_list **alst, t_list *new)
-{
-	t_list	*elem;
-
-	elem = new;
-	elem->next = *alst;
-	*alst = elem;
-}
