6f3c5783b235d3e76694610a2b5342ea32730a92
Junsik Park <junpark@e1z2r7p19.42.us.org>
2019-05-18 17:05:50 -0700

    .

---
+++ solve.c
@@ -0,0 +1,87 @@
+#include "fillit.h"
+
+int		check_place(int i, int j, t_tet *tetris, t_map *map)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < tetris->height)
+	{
+		x = 0;
+		while (x < tetris->width)
+		{
+			if (tetris->str[x][y] == '#' && map->array[x+i][y+j] != '.')
+				return (0);
+			x++;
+		}
+		y++;
+	}
+	put_piece(tetris, map, new_position(x, y), tetris->alpha);
+	return (1);
+}
+
+int		put_piece(t_tet *tetris, t_map *map, t_position *position, char c)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < tetris->height)
+	{
+		x = 0;
+		while (x < tetris->width)
+		{
+			if (tetris->position[x][y] == '#')
+				map->array[position->y + y][position->x + x] = c;
+			x++;
+		}
+		y++;
+	}
+	ft_memdel((void **)&position);
+}
+
+int		rec_backtrack(t_map *map, t_tet *tetlst)
+{
+	int		x;
+	int		y;
+	t_tet	*tetris;
+
+	if(tetlst == NULL)
+		return(0);
+	y = 0;
+	tetris = tetlst;
+	while (y < map->size - tetlst->height + 1)
+	{
+		x = 0;
+		while (x < map->size - tetlst->width + 1)
+		{
+			if (check_place(x, y, tetris, map))
+			{
+				if(rec_backtrack(map, tetlst->next))
+					return (0);
+				else
+					put_piece(tetris, map, new_position(x, y), '.')
+			}
+			x++;
+		}
+		y++;
+	}
+	return(1);
+}
+
+t_map	*solve(t_tet *tetlst)
+{
+	int		x;
+	t_map	map;
+
+	x = 4;
+	map = new_map(x);
+	while (rec_backtrack(map, tetlst))
+	{
+		free_map(&map);
+		x++;
+		map = new_map(x);
+	}
+	return (map);
+}
