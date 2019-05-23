dc4d38e7866ec76c34400f2e208891e77b762f68
atropnik <atropnikova@gmail.com>
2019-05-23 16:46:50 -0700

    finished fillit passing norm

---
+++ fillit_finished/libft/ft_lstrev.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junpark <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 20:27:18 by junpark           #+#    #+#             */
+/*   Updated: 2019/04/11 20:27:24 by junpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_lstrev(t_list **alst)
+{
+	t_list	*prev;
+	t_list	*cur;
+	t_list	*next;
+
+	prev = NULL;
+	cur = *alst;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*alst = prev;
+}
